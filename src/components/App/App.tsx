f033e66de6da1a88ec452e6396be1bc136651a5b
Jacint Blanch <jacintblanch5@gmail.com>
2023-08-18 08:31:28 +0200

    Dispatch action creator

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,19 @@
+import { useAppDispatch } from "../../store";
+import { loadTasksActionCreator } from "../../store/task/TaskSlice";
+import mockedTasks from "../../tasksData";
+import CardList from "../TaskList/CardList";
+
 const App = (): React.ReactElement => {
-  return <p>Hello world!</p>;
+  const dispatch = useAppDispatch();
+
+  dispatch(loadTasksActionCreator(mockedTasks));
+
+  return (
+    <div>
+      <h1>Complete Tasks</h1>
+      <CardList />
+    </div>
+  );
 };
 
 export default App;
